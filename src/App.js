283c23332ef510d3281253c3a2ce771b4f1d9685
Bilal <bilal.se.iiui@gmail.com>
2022-11-16 04:38:33 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useContext, useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import BooksDetails from "./Components/BookDetails/BooksDetails";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import OrderCard from "./Components/OrderCard/OrderCard";
+import firebaseContext from "./Context/Context";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import MyBooksFormPage from "./Pages/MyBooksFormPage/MyBooksFormPage";
+import Order from "./Pages/MyOrders/Order";
+import SignUp from "./Pages/Register/SignUp";
 
 function App() {
+  const [show, setShow] = useState(false);
+  const { AllContext } = useContext(firebaseContext);
+  useEffect(() => {
+    if (AllContext.authUser) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  }, [AllContext]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <ToastContainer />
+
+      <Navbar />
+      <div className="container">
+        <Routes>
+          {show && <Route path="/" element={<Home />} />}
+          {show && <Route path="/book/form" element={<MyBooksFormPage />} />}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/books/details/:booksId" element={<BooksDetails />} />
+          <Route path="orders/book" element={<Order />} />
+          <Route
+            path="orders/book/orders/book/:ordersId"
+            element={<OrderCard />}
+          />
+          {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
+        </Routes>
+      </div>
+      <Footer />
+    </Fragment>
   );
 }
 
